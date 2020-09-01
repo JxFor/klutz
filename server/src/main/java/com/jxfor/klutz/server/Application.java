e3c67f02eb413d16e7f7420a71ee3fedf92116cd
何健 <hej@doordu.com>
2020-09-01 16:57:36 +0800

    创建server模块

---
+++ server/src/main/java/com/jxfor/klutz/server/Application.java
@@ -0,0 +1,16 @@
+package com.jxfor.klutz.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jxfor
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
---
+++ server/src/main/java/com/jxfor/klutz/server/controller/InstanceController.java
@@ -0,0 +1,11 @@
+package com.jxfor.klutz.server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jxfor
+ */
+@RestController
+public class InstanceController {
+}
